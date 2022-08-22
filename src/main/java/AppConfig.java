d6bafb8f601302dc756a93b47532cc612e9ba83f
ErikBonem <yunusoverik@mail.ru>
2022-08-22 18:11:28 +0300

    Initial commit

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,20 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    @Scope("prototype")
+    public Cat catBean(){
+        return new Cat();
+    }
+ 
+    @Bean(name="helloworld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+}
